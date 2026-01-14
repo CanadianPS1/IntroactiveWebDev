01f5d517b98910d380fee04ff9c2df64d0b4852b
Lia <122105453+CanadianPS1@users.noreply.github.com>
2026-01-13 17:55:52 -0700

    got rid of a loop

--- NothingTrivialAboutTrivia/js/index.js
+++ NothingTrivialAboutTrivia/js/index.js
@@ -1,30 +1,36 @@
 const playButton = document.getElementById("playButton");
 const inputField = document.getElementById("input")
 const guessButton = document.getElementById("guessButton")
+let jsonData;
+let iterationCount = 0;
 playButton.addEventListener("click", () =>{
     GetQuestions();
+});
+guessButton.addEventListener("click", () =>{
+    PoseQuestions()
 });
 async function GetQuestions(){
     const amountOfQuestions = document.getElementById("amountOfQuestions").value;
     const difficulty = document.getElementById("difficulty").value;
     const url = `https://opentdb.com/api.php?amount=${amountOfQuestions}&category=15&difficulty=${difficulty}&type=multiple`
     fetch(url).then(response => response.json()).then(data => {
-        PoseQuestions(data);
+        jsonData = data;
+        PoseQuestions(iterationCount);
     });
 }
-function PoseQuestions(jsonData){
+function PoseQuestions(i){
     inputField.style.visibility = "visible";
     guessButton.style.visibility = "visible";
-    for(let i = 0; i < document.getElementById("amountOfQuestions").value; i++){
-        const questionNum = i - 8;
-        const questionNumber = `Question #${questionNum}`;
-        document.getElementById("questionNumberText").textContent = (questionNumber);
-        let question = jsonData.results[i].question;
-        const correctAnswer = jsonData.results[i].correct_answer;
-        jsonData.results[i].incorrect_answers.push(correctAnswer);
-        const answers = jsonData.results[i].incorrect_answers;
-        answers.sort(() => Math.random() - 0.5);
-        document.getElementById("question").textContent = (question);
-        document.getElementById("answers").textContent = (`(a) ${answers[0]}\n(b) ${answers[1]}\n(c) ${answers[2]}\n(d) ${answers[3]}`);
-    }
+    const questionNum = i + 1;
+    const questionNumber = `Question #${questionNum}`;
+    document.getElementById("questionNumberText").textContent = (questionNumber);
+    let question = jsonData.results[i].question;
+    const correctAnswer = jsonData.results[i].correct_answer;
+    jsonData.results[i].incorrect_answers.push(correctAnswer);
+    const answers = jsonData.results[i].incorrect_answers;
+    answers.sort(() => Math.random() - 0.5);
+    document.getElementById("question").textContent = (question);
+    document.getElementById("answers").textContent = (`(a) ${answers[0]}\n(b) ${answers[1]}\n(c) ${answers[2]}\n(d) ${answers[3]}`);
+    console.log(correctAnswer);
+    iterationCount++;
 }
