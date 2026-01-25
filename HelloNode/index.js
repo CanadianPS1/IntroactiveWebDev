49164b2573db7158ebad28bf14f2bfae79385079
Lia <122105453+CanadianPS1@users.noreply.github.com>
2026-01-25 15:44:29 -0700

    did the intro to web devf stuff

--- HelloNode/index.js
+++ HelloNode/index.js
@@ -1,1 +1,8 @@
 console.log("YAR HAR HAR");
+//hello is a command line thing
+//this is the thing for it npx hello CanadianPS1
+const cliSpinners = require('cli-spinners');
+
+console.log(cliSpinners.dots);
+const skinTone = require('skin-tone');
+console.log(skinTone('🦄', 'darkBrown'))
