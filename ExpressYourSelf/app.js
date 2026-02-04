5f51fce40d9764c28e1ce31f5aa736b276713e76
Lia <sethroeback1@gmail.com>
2026-02-04 14:07:31 -0700

    finished the website

--- ExpressYourSelf/app.js
+++ ExpressYourSelf/app.js
@@ -24,9 +24,28 @@ app.get("/orders", (req, res) => {
     res.render("buying");
 });
 app.post("/orders", (req, res) => {
-    let body = req.body;
-    res.redirect("/")
-    console.log("Body" + body);
+    let name = req.body.name;
+    let address = req.body.address;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    console.log("Name: " + name);
+    console.log("Address: " + address);
+    console.log("Email: " + email);
+    console.log("Phone: " + phone);
+    if(name != "" && address != "" && email != "" && phone != ""){
+        const fs = require('fs');
+        res.redirect("/");
+        let orderData = `{ "${name}sOrder" : [` +
+                        `{ "name": "${name}"}, { "address": "${address}"},` +
+                        `{ "email": "${email}"}, { "phone": "${phone}"}]}`;
+        const jsonData = JSON.stringify(orderData, null, 2);
+        fs.writeFile(`${name}sOrder`, jsonData, 'utf8', (err) => {
+            if(err) console.error('Error writing to file', err);
+            else console.log('Data written to file');
+        });
+    }else{
+        res.redirect("/orders");
+    }
 });
 app.listen(PORT, () => {
     console.log("espress running on pork:" + PORT);
