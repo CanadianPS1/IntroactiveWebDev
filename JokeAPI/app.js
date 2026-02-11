cb92996fefad38b435e3b28131ea0ac51b9793aa
Lia <122105453+CanadianPS1@users.noreply.github.com>
2026-02-11 14:27:47 -0700

    did the back end logic

--- JokeAPI/app.js
+++ JokeAPI/app.js
@@ -6,6 +6,24 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
+var leagueJokes = ["Im moving as Fast as I can!!!", 
+                    "Im Zack", 
+                    "Jhin, in the big two six", 
+                    "Misttaaaa YIIII",
+                    "What did the risky gambit say to Majiah : You cost 1150 more gold then me"
+]
+var generalJokes = ["Whys the Grinch so GREEEEN : because he ate all the green juice",
+                    "October 6th 2032, Car Accident.",
+                    "Reply Hazy, try again",
+                    "What you do call a Grinch that isn't green : Orange Grinch ",
+                    "*You enter a room and see a figure looking out the window, his back turned to you* 'Hello?' - you say confused *the man turns around, he appears to be the Grinch* 'Grinch??!' - you esclame *the figure smirks, then takes off his mask, he's a man with a horn coming out of his head and an eye patch* *he looks to the camera still smirking, and invisible by Duran Duran starts playing*"
+]
+var generalerJokes = ["Whats the deal with the my SSD : Like whhyyyyyys it sooo solllidd",
+                        "General. . . the casultys have been massive, we've lost over 70% of our troups",
+                        "DAMN!!! whats the state of the field?",
+                        "we're being pushed back, if this keeps up theyll be at Newyork by 23000",
+                        "*Smashes fist on tabel* call the troups back... we'll defend at Philly, we should be able to regroup and push them back from their."
+]
 app.get("/", (req, res) => {
     res.render("home");
 })
@@ -16,20 +34,13 @@ app.get("/create_joke", (req, res) => {
     res.render("createJoke")
 })
 app.post("/create_joke", (req, res) => {
-    let questionAndAnswer = req.body.questionAndAnswer;
-    let question = req.body.question;
-    let answer = req.body.answer;
     let generalJoke = req.body.generalJoke;
-    console.log("Question and Answer: " + questionAndAnswer);
-    console.log("Question: " + question);
-    console.log("Answer: " + answer);
+    let jokeType = req.body.jokeType;
     console.log("General Joke: " + generalJoke);
-    if(questionAndAnswer && question != "" && answer != "" && generalJoke == ""){
-        const fs = require('fs');
-        res.redirect("/");
-        
-    }else if(!questionAndAnswer && question == "" && answer == "" && generalJoke != ""){
-        const fs = require('fs');
+    if(generalJoke != ""){
+        if(jokeType == "leagueJoke") leagueJokes.push(generalJoke);
+        else if(jokeType == "generalJoke") generalJokes.push(generalJoke);
+        else if(jokeType == "generalerJoke") generalerJokes.push(generalJoke);
         res.redirect("/");
     }else{
         res.redirect("/create_joke");
