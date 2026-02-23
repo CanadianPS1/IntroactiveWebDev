d008448706cf48c391853d381eed649dad428143
Lia <sethroeback1@gmail.com>
2026-02-23 13:40:37 -0700

    tryed to get it working

--- JokeAPIBackend/app.js
+++ JokeAPIBackend/app.js
@@ -46,6 +46,8 @@ app.post("/get_joke", (req, res) => {
         let model = {
             joke: jokeFromDb
         };
+        Response.body = model;
+        Response.ok;
     }catch(err){
         console.error("SERVER ERROR:", err);
         res.status(500).json({ error: err.message });
